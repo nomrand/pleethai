0ee791d0d5518bc82e4103fdb5ffa150840ee3e1
nomrand <42882840+nomrand@users.noreply.github.com>
2020-10-26 22:59:11 +0900

    session key

--- config/settings.py
+++ config/settings.py
@@ -175,5 +175,7 @@ try:
 except ImportError:
     pass
 
+# Heroku Setting
+SECRET_KEY = 'yftgrj&zt8=06mv7(hhum(fjceo2f$3$*xlo7m2gsq&fh92zrf'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
