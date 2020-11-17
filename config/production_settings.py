c4a84d458c55f0070f1ff8d6462e3869ca1ced04
nomrand <42882840+nomrand@users.noreply.github.com>
2020-11-17 19:31:50 +0900

    production_settings.py for about page

---
+++ config/production_settings.py
@@ -0,0 +1,6 @@
+# About Page URL
+ABOUT_URL_MAP = {
+    'ja' : 'https://nomrand.github.io/pleethai/ja/',
+    'en' : 'https://nomrand.github.io/pleethai/en/',
+    'th' : 'https://nomrand.github.io/pleethai/en/',
+}
