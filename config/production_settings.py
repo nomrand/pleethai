dc2224565e1e2ec9449ba50d9d1408d725ec5271
nomrand <42882840+nomrand@users.noreply.github.com>
2021-03-26 14:47:30 +0700

    change production setting to send mail in heroku

--- config/production_settings.py
+++ config/production_settings.py
@@ -1,5 +1,6 @@
 from .settings import *
 import django_heroku
+import os
 
 SECRET_KEY = 'yftgrj&zt8=06mv7(hhum(fjceo2f$3$*xlo7m2gsq&fh92zrf'
 #DEBUG = False
@@ -12,6 +13,20 @@ ABOUT_URL_MAP = {
     'ja' : 'https://nomrand.github.io/pleethai/ja/',
     'en' : 'https://nomrand.github.io/pleethai/en/',
     'th' : 'https://nomrand.github.io/pleethai/th/',
+}
+
+EMAIL_BACKEND = 'gmailapi_backend.mail.GmailBackend'
+GMAIL_API_CLIENT_ID = os.environ['GMAIL_API_CLIENT_ID']
+GMAIL_API_CLIENT_SECRET = os.environ['GMAIL_API_CLIENT_SECRET']
+GMAIL_API_REFRESH_TOKEN = os.environ['GMAIL_API_REFRESH_TOKEN']
+
+REQUEST_MAIL_SEND_INFO = {
+    'subject': 'GaifaaYeepun Request Mail',
+    'templete_path': 'mails/request.txt',
+    'from_email': 'pleethai.jv@gmail.com',
+    'recipient_list': [
+        'pleethai.jv@gmail.com',
+    ],
 }
 
 ### Heroku Setting
